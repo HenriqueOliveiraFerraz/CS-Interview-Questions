f55b6d711fe66d9ba707c663494ed9d4e90f8d2b
HenryFe93 <henri.fna@gmail.com>
2020-03-14 11:39:39 -0300

    fatorial-commit

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número natural(inteiro e positivo) para definir o fatorial do mesmo(método recursão)");
+            Console.WriteLine(FatorialRecursao(int.Parse(Console.ReadLine())).ToString());
+
+            Console.WriteLine("Digite um número natural(inteiro e positivo) para definir o fatorial do mesmo(método iteração)");
+            Console.WriteLine(FatorialIteracao(int.Parse(Console.ReadLine())).ToString());
+        }
+
+        public static int FatorialRecursao(int n)
+        {
+            //Se n for igual a 1 então retorne 1, senão multiplique n pela função Fatorial
+            return n == 1 ? 1 : n * FatorialRecursao(n - 1);
+        }
+
+        public static int FatorialIteracao(int n)
+        {            
+            int fatorial = 1;
+            //Para i igual ao valor de n, i maior ou igual a 1, subtraia i            
+            for (int i = n; i >= 1; i--) 
+            {
+                //assim multiplique a variável fatorial por i e guarde o valor nela mesma
+                fatorial *= i; 
+            }
+            //Se n for igual a 1 então retorne 1, senão retorne o valor da variável fatorial
+            return n == 1 ? 1 : fatorial;                        
+        }
+    }
+}
